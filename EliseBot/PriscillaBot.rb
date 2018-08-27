07fb6b1f7091e6c0b2272e43e1cc67a854880096
Matthew Yarnot <rot8er.conex@gmail.com>
2018-08-27 14:34:15 -0500

    Update PriscillaBot.rb

--- EliseBot/PriscillaBot.rb
+++ EliseBot/PriscillaBot.rb
@@ -15467,7 +15467,9 @@ def next_holiday(bot,mode=0)
             [0,4,24,'Sakura(BDay)','dressup as my best friend.',"Coder's birthday"],
             [0,7,4,'Arthur','for freedom and justice.','Independance Day'],
             [0,8,6,'Zelgius','in recognition of DullahansXMark#9036',"Donator's birthday"],
+            [0,9,16,'Genny','in recognition of Straynine#3480',"Donator's birthday"],
             [0,10,31,'Henry(Halloween)','with a dead Emblian. Nyahaha!','Halloween'],
+            [0,12,6,'Lilina','in recognition of TimDiamond#6094',"Donator's birthday"],
             [0,12,25,'Robin(M)(Winter)','as Santa Claus for Askr.','Christmas'],
             [0,12,31,'Tiki(Adult)','as Mother Time',"New Year's Eve"]]
   for i in 0...holidays.length
