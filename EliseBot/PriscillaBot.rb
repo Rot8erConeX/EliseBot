2070f974bfbf767eca7a408fb97d0c4b3800afc7
Matthew Yarnot <rot8er.conex@gmail.com>
2018-11-21 05:22:22 -0600

    Update PriscillaBot.rb

--- EliseBot/PriscillaBot.rb
+++ EliseBot/PriscillaBot.rb
@@ -1405,71 +1405,35 @@ def find_unit(name,event,ignore=false,ignore2=false) # used to find a unit's dat
     buff=buff[2,buff.length-2] if !event.server.nil? && event.server.id==350067448583553024 && buff[0,2].downcase=='gp'
     name=buff if find_unit(buff,event,ignore)>=0
   end
+  name=name.gsub('(','').gsub(')','') unless ignore2
   untz=@units.map{|q| q}
+  k=untz.find_index{|q| q[0].downcase==name.downcase}
+  k=untz.find_index{|q| q[0].gsub('(','').gsub(')','').downcase==name.downcase} unless ignore2
+  return k unless k.nil?
   unless ignore2
-    for i in 0...@aliases.length # replace any unit aliases with the actual unit name, if possible
-      unless @aliases[i].nil?
-        name=@aliases[i][1] if @aliases[i][0].downcase==name.gsub('(','').gsub(')','').downcase && (@aliases[i][2].nil? || @aliases[i][2].include?(k))
-      end
-    end
+    alz=@aliases.map{|q| q}
+    k=alz.find_index{|q| q[0].downcase==name.downcase && (q[2].nil? || q[2].include?(k))}
+    return untz.find_index{|q| q[0]==alz[k][1]} unless k.nil?
   end
-  j=-1
-  name=name.gsub('!','')
-  name=name.gsub('(','').gsub(')','').gsub('_','') unless ignore2
-  for i in 0...untz.length # try exact unit names
-    unless untz[i].nil?
-      m=untz[i][0]
-      m=m.gsub('(','').gsub(')','') unless ignore2
-      j=i if m.downcase==name.downcase && j<0
-    end
-  end
-  return j if j>=0 && !untz[j].nil? && has_any?(g, untz[j][13][0])
-  return -1 if ignore || name.downcase=='blade' || name.downcase=='blad' || name.downcase=='bla'
-  for i in 0...untz.length # try the portion of a exact unit names that is exactly as long as the input string
-    unless untz[i].nil?
-      m=untz[i][0][0,name.length]
-      m=m.gsub('(','').gsub(')','') unless ignore2
-      j=i if m.downcase==name.downcase && j<0
-    end
-  end
-  return j if j>=0 && !untz[j].nil? && has_any?(g, untz[j][13][0])
+  return -1 if ignore || ['blade','blad','bla'].include?(name.downcase)
+  untz=@units.map{|q| q}
+  k=untz.find_index{|q| q[0][0,name.length].downcase==name.downcase}
+  k=untz.find_index{|q| q[0][0,name.length].gsub('(','').gsub(')','').downcase==name.downcase} unless ignore2
+  return k unless k.nil?
   unless ignore2
-    for i in 0...@aliases.length # try the portion of any alias names that is exactly as long as the input string
-      unless @aliases[i].nil?
-        name=@aliases[i][1] if @aliases[i][0][0,name.length].downcase==name.downcase && (@aliases[i][2].nil? || @aliases[i][2].include?(k)) && find_unit(name,event,true)<0
-      end
-    end
+    alz=@aliases.map{|q| q}
+    k=alz.find_index{|q| q[0][0,name.length].downcase==name.downcase && (q[2].nil? || q[2].include?(k))}
+    return untz.find_index{|q| q[0]==alz[k][1]} unless k.nil?
   end
-  for i in 0...untz.length
-    unless untz[i].nil?
-      m=untz[i][0]
-      m=m.gsub('(','').gsub(')','') unless ignore2
-      j=i if m.downcase==name.downcase && j<0
-    end
-  end
-  return j if j>=0 && !untz[j].nil? && has_any?(g, untz[j][13][0])
   unless ignore2 || !name.downcase.include?('launch')
-    name=name.downcase.gsub('launch','') # if the name includes the word "launch", remove it from consideration\
-    for i in 0...untz.length # try exact unit names
-      unless untz[i].nil?
-        m=untz[i][0]
-        j=i if m.downcase==name.downcase && !m.include?('(') && untz[i][9][0].include?('LU') && j<0 # only units without modifiers in their names, and who are marked as launch units, are considered
-      end
-    end
-    return j if j>=0 && !untz[j].nil? && has_any?(g, untz[j][13][0])
+    name=name.downcase.gsub('launch','') # if the name includes the word "launch", remove it from consideration
+    untz=@units.map{|q| q}
+    k=untz.find_index{|q| q[0].downcase==name.downcase}
+    k=untz.find_index{|q| q[0].downcase==name.downcase && q[9][0].include?('LU')} unless ignore2
+    return k unless k.nil?
     alz=@aliases.reject{|q| q[1].include?('(') || !q[2].nil?}
-    for i in 0...alz.length
-      unless alz[i].nil?
-        name=alz[i][1] if alz[i][0].downcase==name.downcase && untz.find_index{|q| q[0].downcase==name.downcase}.nil?
-      end
-    end
-    for i in 0...untz.length
-      unless untz[i].nil?
-        m=untz[i][0]
-        j=i if m.downcase==name.downcase && !m.include?('(') && untz[i][9][0].include?('LU') && j<0
-      end
-    end
-    return j if j>=0 && !untz[j].nil? && has_any?(g, untz[j][13][0])
+    k=alz.find_index{|q| q[0][0,name.length].downcase==name.downcase}
+    return untz.find_index{|q| q[0]==alz[k][1]} unless k.nil?
   end
   return -1
 end
